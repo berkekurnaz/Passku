a6a623aa364e28093eaafe090d7597559bacd5d9
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-10-04 23:35:38 +0300

    Configure Net Core Mvc App

---
+++ Passku.WebCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Passku.Business.Concrete.MongoDb;
+
+namespace Passku.WebCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        UserManager userManager { get; set; }
+
+        public HomeController(UserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- Passku.WebCoreApp/Startup.cs
+++ Passku.WebCoreApp/Startup.cs
@@ -5,16 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Passku.Business.Concrete.MongoDb;
+using Passku.DataAccess.Concrete.MongoDb;
 
 namespace Passku.WebCoreApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            string mongoConnectionString = this.Configuration.GetConnectionString("MongoConnectionString");
+            services.AddTransient(s => new UserManager(new UserRepository(mongoConnectionString, "DbPasskuApp", "Users")));
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +38,14 @@ namespace Passku.WebCoreApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
         }
     }
 }
