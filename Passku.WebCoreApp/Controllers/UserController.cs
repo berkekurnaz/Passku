1e5427577e258c90a180f7779d037cac02d490cd
BerkeKurnaz <bkoyunberkekurnaz@hotmail.com>
2019-10-07 16:08:26 +0300

    Add Profile Operations For Net Core App

--- Passku.WebCoreApp/Controllers/UserController.cs
+++ Passku.WebCoreApp/Controllers/UserController.cs
@@ -29,14 +29,72 @@ namespace Passku.WebCoreApp.Controllers
             return View();
         }
 
-
-
+      
+    
         /* List Passwords By UserId */
         public IActionResult Passwords()
         {
             var userId = HttpContext.Session.GetString("SessionUserId").ToString();
             var list = storedPasswordManager.GetByUserId(userId);
             return View(list);
+        }
+
+
+
+        /* List Announcements */
+        public IActionResult Announcements()
+        {
+            // BURAYA DUYURULARI LİSTELE.
+            return View();
+        }
+
+        /* Get Announcement By Announcement Id */
+        public IActionResult AnnouncementDetail(string id)
+        {
+            // BURADA ID DEGERİNE GORE DUYURU GELECEK.
+            return View();
+        }
+
+
+
+        /*---------------------------------------------------------------------------*/
+
+        /* List Passwords By UserId */
+        public IActionResult Profile()
+        {
+            var userId = HttpContext.Session.GetString("SessionUserId").ToString();
+            var user = userManager.GetById(userId);
+            return View(user);
+        }
+
+        /* Change Password Operations */
+        [HttpPost]
+        public IActionResult ChangePassword(string oldPassword, string newPassword1, string newPassword2)
+        {
+            var userId = HttpContext.Session.GetString("SessionUserId").ToString();
+            var user = userManager.GetById(userId);
+            if (oldPassword.Length >= 2 && newPassword1.Length >= 2 && newPassword2.Length >= 2)
+            {
+                if (user.Password == oldPassword)
+                {
+                    if (newPassword1 == newPassword2)
+                    {
+                        user.Password = newPassword1;
+                        userManager.Update(userId, user);
+                        TempData["MsgUserChangePasswordSuccesful"] = "Your password has been successfully updated.";
+                        return RedirectToAction("Profile");
+                    }
+                    else
+                    {
+                        TempData["MsgUserChangePasswordError"] = "The two passwords you typed are not the same.";
+                        return RedirectToAction("Profile");
+                    }
+                }
+                TempData["MsgUserChangePasswordError"] = "Please enter the correct password you are currently using";
+                return RedirectToAction("Profile");
+            }
+            TempData["MsgUserChangePasswordError"] = "Please enter the required fields";
+            return RedirectToAction("Profile");
         }
 
 
